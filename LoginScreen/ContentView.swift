c42fac1b5968fc04318a4b2909d37f074bd1d022
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-12-02 20:14:46 -0300

    First Commit

--- LoginScreen/ContentView.swift
+++ LoginScreen/ContentView.swift
@@ -8,15 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var alertMessage: String = ""
+    @State private var showAlert: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Usuário", text: $username).frame(maxWidth: 100)
+            SecureField("Senha", text: $password).frame(maxWidth: 100)
+            Button("Login", action: handleLogin)
+            
+            Button("Esqueci a senha") {
+                alertMessage = "Função de recuperação de senha não implementada."
+                showAlert = true
+            }
         }
         .padding()
+        .alert(alertMessage, isPresented: $showAlert) {
+            Button("OK", role: .cancel) { }
+        }
     }
+    private func handleLogin() {
+            if username == "Admin" && password == "123" {
+                alertMessage = "Login feito com sucesso!"
+            } else {
+                alertMessage = "Credenciais invalidas"
+            }
+            showAlert = true
+        }
 }
 
 #Preview {
