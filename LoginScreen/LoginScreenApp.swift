a781f7def7a281d7f6e42b41b1271823c1158a24
paulomatheus <paulomatheus.araujo@hotmail.com>
2025-12-02 19:27:49 -0300

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Paulo Matheus on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
